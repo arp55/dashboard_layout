644ea3e7d53d037e1e2b0a1a7a8bdf366e5f4046
Adithya R Pai <46337104+arp55@users.noreply.github.com>
2022-06-29 16:37:13 +0530

    height changed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,52 +1,53 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import SideBar from './sidebar';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import ChatScreen from './chatscreen';
-import InfoScreen from './infoscreen'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SideBar from "./sidebar";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import ChatScreen from "./chatscreen";
+import InfoScreen from "./infoscreen";
 
 const useStyles = makeStyles({
-    root: {
-        display: 'flex',
-        height: 'fill-available',
-    },
-    drawer: {
-        minWidth: 547,
-    },
-    drawerLessWidth: {
-        minWidth: 82
-    },
-    content: {
-        flex: '1 0 auto',
-        backgroundColor: '#eeeeee'
-    },
-    info: {
-        flex: '1 0 auto',
-        maxWidth: 275,
-        textAlign: 'center'
-    },
-    infoLessWidth: {
-        display: 'none'
-    }
+  root: {
+    display: "flex",
+    height: "fill-available",
+  },
+  drawer: {
+    minWidth: 547,
+    height: "100vh",
+  },
+  drawerLessWidth: {
+    minWidth: 82,
+  },
+  content: {
+    flex: "1 0 auto",
+    backgroundColor: "#eeeeee",
+  },
+  info: {
+    flex: "1 0 auto",
+    maxWidth: 275,
+    textAlign: "center",
+  },
+  infoLessWidth: {
+    display: "none",
+  },
 });
 
 export default function Layout() {
-    const classes = useStyles();
-    const matches = useMediaQuery('(min-width:769px)');
+  const classes = useStyles();
+  const matches = useMediaQuery("(min-width:769px)");
 
-    return (
-        <>
-            <div className={classes.root}>
-                <div className={matches ? classes.drawer : classes.drawerLessWidth}>
-                    <SideBar />
-                </div>
-                <div className={classes.content}>
-                    <ChatScreen />
-                </div>
-                <div className={matches ? classes.info : classes.infoLessWidth}>
-                    <InfoScreen />
-                </div>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={classes.root}>
+        <div className={matches ? classes.drawer : classes.drawerLessWidth}>
+          <SideBar />
+        </div>
+        <div className={classes.content}>
+          <ChatScreen />
+        </div>
+        <div className={matches ? classes.info : classes.infoLessWidth}>
+          <InfoScreen />
+        </div>
+      </div>
+    </>
+  );
 }
